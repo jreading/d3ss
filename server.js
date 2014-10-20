974789791a6e7175ff5900b87d03fbd7db2c2543
Johnny Reading <jreading@hellowallet.com>
2014-10-20 10:51:51 -0400

    rendering image and raw nodes

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var connect = require('connect'),
+    serveStatic = require('serve-static'),
+    compression = require('compression'),
     http = require('http'),
     d3 = require('d3'),
     jsdom = require('jsdom'),
-    fs = require('fs');
+    fs = require('fs'),
+    phantom = require('phantom');
 
-var app = connect();
-var port = 1337;
-var htmlStub = '<html><body><div id="main"></div></body></html>';
-var file;
-
+var app = connect(),
+    port = 1337,
+    htmlStub = '<html><body><div id="main"></div></body></html>',
+    raw = serveStatic('raw', {'index': ['index.html', 'index.htm']}),
+    rendered = serveStatic('rendered', {'index': ['rendered.png']}),
+    file;
 
 // gzip/deflate outgoing responses
-var compression = require('compression');
 app.use(compression());
 
 // pass the html stub to jsDom
@@ -23,6 +26,7 @@ jsdom.env({
     done: function(errors, window) {
         var body = window.document.querySelector('body');
         var el = window.document.querySelector('#main');
+        
         d3.select(el)
             .append('svg:svg')
             .attr('width', 600).attr('height', 300)
@@ -30,14 +34,36 @@ jsdom.env({
                 .attr('cx', 300).attr('cy', 150).attr('r', 30).attr('fill', '#26963c');
 
         file = window.document.documentElement.outerHTML;
+        fs.writeFile('raw/index.html', file, function(err) {
+            if(err) {
+                console.log('error saving document', err);
+            } else {
+                console.log('Raw file was saved');
+            }
+        });
+        app.use('/raw', raw);
+
+        phantom.create(function (ph) {
+            ph.createPage(function (page) {
+                page.open('http://localhost:1337/raw', function (status) {
+                    page.render('rendered/rendered.png', {format: 'png', quality: '100'}, function() {
+                        console.log('Rendered file was saved');
+                        ph.exit();
+                    });
+                });
+            });
+        });
+        app.use('/rendered', rendered);
     }
 });
 
-// respond to all requests
-app.use(function(req, res){
-  res.end(file);
-});
+app.listen(port);
+console.log('Listening on ' + port);
 
-//create node.js http server and listen on port
-http.createServer(app).listen(port);
-console.log("Listening on " + port);
+
+
+// page.open('/raw', function (status) {
+//     page.render('/rendered/rendered.png', {format: 'png', quality: '100'});
+//     phantom.exit();
+// });
+
