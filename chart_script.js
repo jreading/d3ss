1b02a6f10583a6713fa9fc875445abe4f99561d5
John Reading <jreading@Johnnys-MBP.local>
2014-10-21 15:02:48 -0400

    tweaks

--- chart_script.js
+++ chart_script.js
@@ -11,13 +11,16 @@ setTimeout(function() {
         .transition()
         .duration(750)
         .call(arcTween, (score/100) * tau);
+
+    d3.select("#text")
+        .transition()
+        .duration(750)
+        .attr("fill", bgColor);
 }, 500);
 
 function arcTween(transition, newAngle) {
   transition.attrTween("d", function(d) {
-    console.log(d);
     var interpolate = d3.interpolate(0, newAngle);
-
     return function(t) {
       d.endAngle = interpolate(t);
       return arc(d);
